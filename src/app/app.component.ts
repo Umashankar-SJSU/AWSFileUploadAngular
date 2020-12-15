e2a600ab3818396fe50ba5f70b759197e054172f
Umashankar-SJSU <uma.sjsu0720@gmail.com>
2020-12-15 01:35:53 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CMPE-281 Aws assignment - Monitor objects in S3 Bucket';
+  
+}
